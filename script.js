7e693926bcd14287807183e52d376aaeaa5e98c5
Fortune Alexander <111578125+Halexxander@users.noreply.github.com>
2023-06-25 21:32:12 +0100

    Command v5

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let background = document.getElementById('background-container')
-background.addEventListener = (() => {
-    
-})
+let background = document.getElementById("background-container");
+background.addEventListener = () => {};
+
+window.addEventListener("load", function () {
+  var loader = document.getElementById("loader-container");
+  var video = document.getElementById("loader-video");
+
+  video.addEventListener("loadedmetadata", function () {
+    setTimeout(function () {
+      loader.style.display = "none";
+    }, video.duration * 1000); // Multiply by 1000 to convert seconds to milliseconds
+  });
+});
+
